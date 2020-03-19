2e6c7f354e53d843a92cc74f36cc7621a34081f4
MRIOSAPUTRA-191030300010 <62367983+MRIOSAPUTRA-191030300010@users.noreply.github.com>
2020-03-19 11:00:38 +0700

    Add files via upload

---
+++ cin.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	int jumlah= 20;
+	float penghuni = 5;
+	char kos_terletak = '9';
+	string pemilik = "rio";
+	bool orang_kediri = false;
+	
+	cout <<"jumlah kos nya adalah "<<jumlah<<endl;
+	cout <<"kos tersebut berpenghuni "<<penghuni<<endl;
+	cout <<"kos tersebut terletak di " <<kos_terletak<<endl;
+	cout <<"nama pemilik kos tersebut adalah " <<pemilik<<endl;
+	cout <<"apakah pemilik kos tersebut orang kediri " <<orang_kediri<<endl;
+	
+	
+	return 0;
+}
